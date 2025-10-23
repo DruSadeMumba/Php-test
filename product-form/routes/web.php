76458f5671a405d61b67e90070b64529fac59933
Dru Mumba <drusademumba@gmail.com>
2025-10-23 15:11:25 +0300

    Add routes

--- product-form/routes/web.php
+++ product-form/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
