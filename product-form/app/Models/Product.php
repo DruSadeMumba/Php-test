d552193a6eafeeb1c832b3f661781671627f31ab
Dru Mumba <drusademumba@gmail.com>
2025-10-23 14:51:08 +0300

    Create Product model

---
+++ product-form/app/Models/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+    namespace Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
+
+    class Product extends Model
+    {
+        use HasFactory;
+        protected $fillable = [
+            'name',
+            'quantity',
+            'price',
+        ];
+    }
